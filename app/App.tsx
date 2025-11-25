5134dd154d7bcee0f4cfc7f7d4622a409157d010
Danyaell <danyaellmtzo17@gmail.com>
2025-11-24 19:14:55 -0600

    clean-project

---
+++ app/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Vida Plena</Text>
+      <Text style={styles.subtitle}>
+        App de Salud para Adultos Mayores.
+      </Text>
+      <StatusBar style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
